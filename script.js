c588112a64e57a197353dbff66afbe2aa6cc9621
Clement Uzochukwu Onuocha(clementouzo66) <clementouzo66@gmail.com>
2025-02-28 18:44:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var btn=document.querySelector('.btn');
-var menu=document.querySelector('.menu');
-btn.addEventListener('click',function(){
+var btn = document.querySelector('.btn');
+var menu = document.querySelector('.menu');
+
+
+btn.addEventListener('click',function () {
     menu.classList.toggle('active');
+
 });
-var navlinks=document.querySelector('ul li a');
-for(var i=0;i<navlinks.length;i++){
-    navlinks[i].addEventListener('click',function()
-    {
-        menu.classList.remove('active');
-    })
-}   
+
+
+var navlinks = document.querySelectorAll('ul li');
+navlinks.forEach(function (link) {
+    navlink[i].addEventListener('click', function () {
+        menu.classList.remove('active')
+    });
+});
